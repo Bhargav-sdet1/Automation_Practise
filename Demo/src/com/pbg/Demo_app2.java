0511da472a88f7dfa3d657a8c4fc7d02b669ddc4
Bhargav-sdet1 <bhargav.sdet1@gmail.com>
2023-08-08 00:06:58 +0530

    Add1Prog

---
+++ Demo/src/com/pbg/Demo_app2.java
@@ -0,0 +1,15 @@
+package com.pbg;
+
+public class Demo_app2 {
+
+	public static void main(String[] args) {
+		String name="Hello world welcome";
+		
+		String[] str= name.split(" ");
+		System.out.println(str[0]);
+		System.out.println(str[1]);
+		System.out.println(str[2]);
+
+	}
+
+}
