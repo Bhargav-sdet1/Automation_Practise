93374de647be32c36b0a300099edc3dc9f932aee
Bhargav-sdet1 <bhargav.sdet1@gmail.com>
2023-08-15 23:46:18 +0530

    Pro

--- SeleniumFrameworkDesign/src/test/java/RSA/SeleniumFrameworkDesign/Standalone.java
+++ SeleniumFrameworkDesign/src/test/java/RSA/SeleniumFrameworkDesign/Standalone.java
@@ -1,9 +1,11 @@
 package RSA.SeleniumFrameworkDesign;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Standalone {
@@ -15,9 +17,26 @@ public class Standalone {
 		driver.get("https://rahulshettyacademy.com/client/");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		//Login
-		driver.findElement(By.id("userEmail")).sendKeys("nov9@yopmail.com");
+		driver.findElement(By.id("userEmail")).sendKeys("ram@yopmail.com");
 		driver.findElement(By.id("userPassword")).sendKeys("Asdfg1@34");
 		driver.findElement(By.name("login")).click();
+		
+		//Locating all products
+		
+		//List<WebElement> allProducts=driver.findElements(By.xpath("//div[contains(@class,'mb-3')]"));
+		
+		/*WebElement prod=allProducts.stream()
+		.filter(product->product
+		.findElement(By.xpath("//div[contains(@class,'mb-3')]//b"))
+		.getText().equals("ADIDAS ORIGINAL")).findFirst().orElse(null);*/
+		//prod.findElement(By.xpath("//button[contains(@class,'w-10')]")).click();
+		
+		List<WebElement> allProducts=driver.findElements(By.cssSelector(".mb-3"));
+		WebElement prod=allProducts.stream()
+				.filter(product->product
+				.findElement(By.cssSelector("b"))
+				.getText().equals("ADIDAS ORIGINAL")).findFirst().orElse(null);
+		prod.findElement(By.cssSelector(".w-10")).click();
 		
 	}
 
