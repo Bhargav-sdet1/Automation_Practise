abac15c73cd87d52c8e2b68e6fbb66b740910760
Bhargav-sdet1 <bhargav.sdet1@gmail.com>
2023-08-16 01:18:44 +0530

    Standalone Completed

--- SeleniumFrameworkDesign/src/test/java/RSA/SeleniumFrameworkDesign/Standalone.java
+++ SeleniumFrameworkDesign/src/test/java/RSA/SeleniumFrameworkDesign/Standalone.java
@@ -4,9 +4,13 @@ import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 public class Standalone {
 	
@@ -30,13 +34,40 @@ public class Standalone {
 		.findElement(By.xpath("//div[contains(@class,'mb-3')]//b"))
 		.getText().equals("ADIDAS ORIGINAL")).findFirst().orElse(null);*/
 		//prod.findElement(By.xpath("//button[contains(@class,'w-10')]")).click();
-		
+		String addedProduct="ADIDAS ORIGINAL";
 		List<WebElement> allProducts=driver.findElements(By.cssSelector(".mb-3"));
 		WebElement prod=allProducts.stream()
 				.filter(product->product
 				.findElement(By.cssSelector("b"))
-				.getText().equals("ADIDAS ORIGINAL")).findFirst().orElse(null);
+				.getText().equals(addedProduct)).findFirst().orElse(null);
 		prod.findElement(By.cssSelector(".w-10")).click();
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#toast-container")));
+		wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(".ng-animating"))));
+		
+		driver.findElement(By.xpath("//button[@routerlink='/dashboard/cart']")).click();
+		
+		List<WebElement> cartItems=driver.findElements(By.cssSelector(".infoWrap"));
+		Boolean bool=cartItems.stream().anyMatch(pro->pro.findElement(By.cssSelector("h3")).getText().equals(addedProduct));
+		Assert.assertTrue(bool);
+		driver.findElement(By.cssSelector(".totalRow .btn-primary")).click();
+		
+		driver.findElement(By.cssSelector("[placeholder='Select Country']")).sendKeys("India");
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button.ta-item")));
+	
+		
+		driver.findElement(By.xpath("//button[contains(@class,'ta-item')][2]")).click();
+		
+		JavascriptExecutor js=(JavascriptExecutor) driver;
+		js.executeScript("window.scrollBy(0,700)");
+		WebElement placeOrder=driver.findElement(By.xpath("//*[@class='actions']//a"));
+		js.executeScript("arguments[0].click();", placeOrder);
+		
+		String ConfirmMsg=driver.findElement(By.xpath("//td/h1")).getText();
+		Assert.assertTrue(ConfirmMsg.equalsIgnoreCase("THANKYOU FOR THE ORDER."));
+		
+		driver.close();	
 		
 	}
 
